2b796e6dfa38ad1202500118f2066d561de4d628
LuisCarlosLHdz <111992297+LuisCarlosLHdz@users.noreply.github.com>
2026-06-01 12:47:11 -0600

    Update conversion from Q6.25 to Q3.28 format

--- FixedPointGraphs .py
+++ FixedPointGraphs .py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 ############################
-#Función para convertir un número en formato Q6.25 a decimal
-# Function to convert a number in Q6.25 format to decimal
+#Función para convertir un número en formato Q3.28 a decimal
+# Function to convert a number in Q3.28 format to decimal
 def hex_to_FPDec(hex_str, signed=True):
     total_bits = 32 #Ancho de palabra/Word size
     frac_bits = 28 #Número de bits para la parte fraccionaria/Number of bits for the fractional part
@@ -26,34 +26,33 @@ print(fixed_values)
 df = pd.read_csv("data_Lu.txt", header=None, sep=',', encoding='utf-8') #Leer datos del archivo: data_Lu.txt/ Read data from the file: data_Lu.txt
 n = len(df) #Tamaño del dataframe / Size of the dataframe
 var = 3; #Número de variables / Number of variables
-q625_to_dec = np.zeros((n,var)) #Crear un arreglo de índices / Create an array of indices
+q328_to_dec = np.zeros((n,var)) #Crear un arreglo de índices / Create an array of indices
 for i in range(var):
     for j in range(n):
-        q625_to_dec[j][i] = hex_to_FPDec(df[i][j])
+        q328_to_dec[j][i] = hex_to_FPDec(df[i][j])
 ###############################
 with open("matriz.txt", "w") as f:
-    for fila in q625_to_dec:
+    for fila in q328_to_dec:
         linea = ", ".join(f"{x:.6f}" for x in fila)
         f.write(linea + "\n")
 
 ################################
 #Graficar los datos / Plot the data
 plt.subplot(2, 2, 1)
-plt.plot(q625_to_dec[:, 0], q625_to_dec[:, 1], linestyle='-', color='black', label='Q3,28')
+plt.plot(q328_to_dec[:, 0], q328_to_dec[:, 1], linestyle='-', color='black', label='Q3,28')
 plt.xlabel("x1")
 plt.ylabel("x2")
 plt.grid(True)
 plt.subplot(2, 2, 2)
-plt.plot(q625_to_dec[:, 0], q625_to_dec[:, 2], linestyle='-', color='black', label='Q3,28')
+plt.plot(q328_to_dec[:, 0], q328_to_dec[:, 2], linestyle='-', color='black', label='Q3,28')
 plt.xlabel("x1")
 plt.ylabel("x3")
 plt.grid(True)
 plt.subplot(2, 2, 3)
-plt.plot(q625_to_dec[:, 1], q625_to_dec[:, 2], linestyle='-', color='black', label='Q3,28')
+plt.plot(q328_to_dec[:, 1], q328_to_dec[:, 2], linestyle='-', color='black', label='Q3,28')
 plt.xlabel("x1")
 plt.ylabel("x4")
 plt.grid(True)
 
 plt.show()
 ####################
-
